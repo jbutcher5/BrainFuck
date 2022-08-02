54f07f30e252613b9b4d07aed56c457e457ade56
James Butcher <jamesbutcher167@gmail.com>
2022-08-02 20:33:19 +0100

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,50 @@
+module Main where
+
+initial :: String
+initial = "section .text\n\
+              \   global _start\n\n\
+          \_start:\n\
+          \    mov r8, msg\n"
+
+final :: String
+final = "\nsection .bss\n\
+        \    msg:    resb 1024"
+
+output :: String
+output = "\n    mov rax, 1\n\
+         \    mov rdi, 1\n\
+         \    mov rsi, r8\n\
+         \    mov rdi, 1\n"
+
+inc :: String
+inc = "\n    mov al, [r8]\n\
+      \    inc al\n\
+      \    mov [r8], al\n"
+
+dec :: String
+dec = "\n    mov al, [r8]\n\
+      \    dec al\n\
+      \    mov [r8], al\n"
+
+moveright :: String
+moveright = "\n    inc r8\n"
+
+moveleft :: String
+moveleft = "\n    dec r8\n"
+
+resolveToken :: Char -> String
+resolveToken '+' = inc
+resolveToken '-' = dec
+resolveToken '>' = moveright
+resolveToken '<' = moveleft
+resolveToken '.' = output
+resolveToken x = [x]
+
+f :: Char -> String -> String
+f x y = y ++ resolveToken x
+
+generateAsm :: String -> String
+generateAsm input = initial ++ foldr f "" input ++ final
+
+main :: IO ()
+main = putStrLn $ generateAsm "+."
