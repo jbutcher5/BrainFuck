ea50fae1c0ae248cbeb1ec1384ab0bffbc291462
James Butcher <jamesbutcher167@gmail.com>
2022-08-03 16:21:58 +0100

    Swap left and right idents

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,8 @@ import Lib
 generateAsm' :: String -> Int -> [Int] -> String -> String
 generateAsm' ('+':xs) n stack acc = generateAsm' xs n stack (acc ++ inc)
 generateAsm' ('-':xs) n stack acc = generateAsm' xs n stack (acc ++ dec)
-generateAsm' ('<':xs) n stack acc = generateAsm' xs n stack (acc ++ moveRight')
-generateAsm' ('>':xs) n stack acc = generateAsm' xs n stack (acc ++ moveLeft')
+generateAsm' ('<':xs) n stack acc = generateAsm' xs n stack (acc ++ moveLeft')
+generateAsm' ('>':xs) n stack acc = generateAsm' xs n stack (acc ++ moveRight')
 generateAsm' ('.':xs) n stack acc = generateAsm' xs n stack (acc ++ output)
 generateAsm' ('[':xs) n stack acc = generateAsm' xs (n + 1) (stack ++ [n]) (acc ++ loopStart n)
 generateAsm' (']':xs) n (x:xs') acc = generateAsm' xs n xs' (acc ++ loopEnd x)
