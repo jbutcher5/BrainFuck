b41d6116bd3bc5cd336695fda9e0d82d5c993e89
James Butcher <jamesbutcher167@gmail.com>
2022-08-03 13:54:00 +0100

    Support nested loops

--- app/Main.hs
+++ app/Main.hs
@@ -50,20 +50,19 @@ moveRight' = "\n    inc r8\n"
 moveLeft' :: String
 moveLeft' = "\n    dec r8\n"
 
-generateAsm' :: String -> Int -> String -> String
-generateAsm' (x:xs) n acc = case x of
-  '+' -> generateAsm' xs n (acc ++ inc)
-  '-' -> generateAsm' xs n (acc ++ dec)
-  '>' -> generateAsm' xs n (acc ++ moveRight')
-  '<' -> generateAsm' xs n (acc ++ moveLeft')
-  '.' -> generateAsm' xs n (acc ++ output)
-  '[' -> generateAsm' xs (n + 1) (acc ++ loopStart n)
-  ']' -> generateAsm' xs n (acc ++ loopEnd (n - 1))
-  _ -> generateAsm' xs n acc
-generateAsm' "" _ result = result
+generateAsm' :: String -> Int -> [Int] -> String -> String
+generateAsm' ('+':xs) n stack acc = generateAsm' xs n stack (acc ++ inc)
+generateAsm' ('-':xs) n stack acc = generateAsm' xs n stack (acc ++ dec)
+generateAsm' ('<':xs) n stack acc = generateAsm' xs n stack (acc ++ moveRight')
+generateAsm' ('>':xs) n stack acc = generateAsm' xs n stack (acc ++ moveLeft')
+generateAsm' ('.':xs) n stack acc = generateAsm' xs n stack (acc ++ output)
+generateAsm' ('[':xs) n stack acc = generateAsm' xs (n + 1) (stack ++ [n]) (acc ++ loopStart n)
+generateAsm' (']':xs) n (x:xs') acc = generateAsm' xs n xs' (acc ++ loopEnd x)
+generateAsm' (_:xs) n stack acc = generateAsm' xs n stack acc
+generateAsm' "" _ _ result = result
 
 generateAsm :: String -> String
-generateAsm input = initial ++ generateAsm' input 0 "" ++ final
+generateAsm input = initial ++ generateAsm' input 0 [] ("" ++ final)
 
 main :: IO ()
 main = putStrLn . generateAsm $ ">++++++++[<+++++++++>-]<."
