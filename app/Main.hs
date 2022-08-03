1a81af202f6845dd0dc402747cf86f91abc80a1b
James Butcher <jamesbutcher167@gmail.com>
2022-08-03 01:09:17 +0100

    Reformat strings

--- app/Main.hs
+++ app/Main.hs
@@ -15,34 +15,34 @@ loopEnd x = "\n    mov al, [r8]\n"
   where y = show x
 
 initial :: String
-initial = "section .text\n\
-              \   global _start\n\n\
-          \_start:\n\
-          \    mov r8, msg\n"
+initial = "section .text\n"
+  ++ "   global _start\n\n"
+  ++ "_start:\n"
+  ++ "    mov r8, msg\n"
 
 final :: String
-final = "\n    mov rax, 60\n\
-        \    mov rdi, 0\n\
-        \    syscall\n\n\
-        \section .bss\n\
-        \    msg:    resb 1024"
+final = "\n    mov rax, 60\n"
+  ++ "    mov rdi, 0\n"
+  ++ "    syscall\n\n"
+  ++ "section .bss\n"
+  ++ "    msg:    resb 1024"
 
 output :: String
-output = "\n    mov rax, 1\n\
-         \    mov rdi, 1\n\
-         \    mov rsi, r8\n\
-         \    mov rdx, 1\n\
-         \    syscall\n"
+output = "\n    mov rax, 1\n"
+  ++ "    mov rdi, 1\n"
+  ++ "    mov rsi, r8\n"
+  ++ "    mov rdx, 1\n"
+  ++ "    syscall\n"
 
 inc :: String
-inc = "\n    mov al, [r8]\n\
-      \    inc al\n\
-      \    mov [r8], al\n"
+inc = "\n    mov al, [r8]\n"
+  ++ "    inc al\n"
+  ++ "    mov [r8], al\n"
 
 dec :: String
-dec = "\n    mov al, [r8]\n\
-      \    dec al\n\
-      \    mov [r8], al\n"
+dec = "\n    mov al, [r8]\n"
+  ++ "    dec al\n"
+  ++ "    mov [r8], al\n"
 
 moveRight' :: String
 moveRight' = "\n    inc r8\n"
